5e057bc064d4c1a73ec7e7f760b29cb87993e44e
Alex Castillo <alex.jc7516@gmail.com>
2023-11-06 10:47:08 -0700

    testing

--- src/examples/counter/test/setup.ts
+++ src/examples/counter/test/setup.ts
@@ -1,1 +1,7 @@
 import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
+
+import { expect } from 'vitest';
+import matchers from '@testing-library/jest-dom/matchers';
+
+expect.extend(matchers);
--- src/examples/counter/test/utilities.ts
+++ src/examples/counter/test/utilities.ts
@@ -1,3 +1,17 @@
+import { render as renderComponent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+// renders components and creates a user
+export const render = (
+  ui: React.ReactElement,
+  options?: Parameters<typeof renderComponent>[1],
+) => {
+  return {
+    ...renderComponent(ui, options),
+    user: userEvent.setup(),
+  };
+};
+
 export {};
 
 /**
